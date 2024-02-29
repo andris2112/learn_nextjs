104cde4dff1d54ed6a61e3e0c1ca4f2099ee7420
andris2112 <andriskokins12@gmail.com>
2024-02-29 22:13:45 +0000

    Add login page

--- nextjs-dashboard/auth.ts
+++ nextjs-dashboard/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { authConfig } from './auth.config';
+import { authConfig } from './app/auth.config';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
@@ -29,9 +29,9 @@ export const { auth, signIn, signOut } = NextAuth({
                     const { email, password } = parsedCredentials.data;
                     const user = await getUser(email);
                     if (!user) return null;
-                    const passwordsMatch = await bcrypt.compare(password, user.password);
+                    const passwordMatch = await bcrypt.compare(password, user.password);
 
-                    if (passwordsMatch) return user;
+                    if (passwordMatch) return user;
                 }
 
                 console.log('Invalid credentials');
